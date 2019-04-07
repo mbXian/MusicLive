c30ea1be1a6b8c007b603bd481308a1de32c734e
Ben <mbxian@xtremeprog.com>
2019-04-08 00:02:53 +0800

    update

--- script.js
+++ script.js
@@ -26,16 +26,16 @@ function Song(id, singer, name, url){
 function initMunSongs() {
   var songList = new Array();
   
-  let song0 = new Song(0, '陈慧娴', '千千阕歌', 'http://fs.open.kugou.com/ef58542e3f62fe98ebf99df55cb0ccd3/5ca5e2b2/G126/M0A/12/11/vg0DAFpJ_WKAeoQNACcP2_ElSwo594.mp3');
+  let song0 = new Song(0, '陈慧娴', '千千阕歌', 'http://fs.open.kugou.com/a861606957a7c29f8c418456bfd1e33a/5caa1bc7/G126/M0A/12/11/vg0DAFpJ_WKAeoQNACcP2_ElSwo594.mp3');
   songList.push(song0);
   
-  let song1 = new Song(1, '黎瑞恩', '一人有一个梦想', 'http://fs.open.kugou.com/0d3144bbbed9d3196f04fdf4fa32a4ff/5ca5e2e6/G105/M02/09/05/CYcBAFvc3kOAPUUVADjDCMHBtaw694.mp3')
+  let song1 = new Song(1, '黎瑞恩', '一人有一个梦想', 'http://fs.open.kugou.com/21a852106f80bb943eed0ae31b5f3d13/5caa1e07/G049/M09/1C/0D/cQ0DAFYvSi2Aeov8ACW5rW3QZAc691.mp3')
   songList.push(song1);
   
-  let song3 = new Song(2, '谭咏麟', '讲不出再见', 'http://fs.open.kugou.com/dc7bc5a28be70384644157cd1d534dd8/5ca5e9f9/G142/M06/04/08/bpQEAFuKhOyAJS0sAEi4t--F9T0544.mp3')
+  let song3 = new Song(2, '谭咏麟', '讲不出再见', 'http://fs.open.kugou.com/d8bc48700979a93e32f8bfeec6d0d19f/5caa1e2c/G101/M00/05/10/RZQEAFkB4_6AZs9kAE_Zu2DQKIg889.mp3')
   songList.push(song3);
   
-  let song4 = new Song(3, '关正杰', '俩忘烟水里', 'http://fs.open.kugou.com/ac8bf8890f1491594e7d92308a3da2bb/5ca5ea2e/G065/M01/17/19/gQ0DAFeaawSAKXWAAC-bCmXhS5Y344.mp3')
+  let song4 = new Song(3, '关正杰', '俩忘烟水里', 'http://fs.open.kugou.com/f91ca9164b4505aeeadb529e147ea1ea/5caa1bdc/G032/M03/18/11/AJQEAFXkx-WAJYsPAC3e5D88CAg064.mp3')
   songList.push(song4);
   
   return songList;
@@ -44,10 +44,10 @@ function initMunSongs() {
 function initSonSongs() {
   var songList = new Array();
 
-  let song0 = new Song(0, '儿童', '一闪一闪亮晶晶', 'http://fs.open.kugou.com/a9a121a28b149fd245065ac71b1a5c98/5ca5e242/G057/M0A/0E/00/eQ0DAFawAM-AeoQjABmRiFIBeq8524.mp3');
+  let song0 = new Song(0, '杨烁', '两只老虎', 'http://fs.open.kugou.com/5a999c9821e4de95e17966c0b2a0a798/5caa1ecd/G081/M01/18/11/MZQEAFhYrl2ARtxhABaVpWSFcuc063.mp3');
   songList.push(song0);
 
-  let song1 = new Song(1, '杨烁', '两只老虎', 'http://fs.open.kugou.com/60c8e9fc557a88166d44f686b945b011/5ca5e271/G124/M02/09/07/vA0DAFo7eXCAPSVPABGk6JgmntY967.mp3')
+  let song1 = new Song(1, '儿童', '一闪一闪亮晶晶', 'http://fs.open.kugou.com/3a4524ffc64a1281162bf331b1956ac6/5caa1f0c/G135/M07/1D/14/Z5QEAFtoOR-AKfwqABUEOU2w8Ak597.mp3');
   songList.push(song1);
 
   return songList;
