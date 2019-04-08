e93e41100cf4a0f7996562dcc8f0fd4916a99ddc
Ben <mbxian@xtremeprog.com>
2019-04-08 17:27:47 +0800

    update song url

--- script.js
+++ script.js
@@ -26,16 +26,16 @@ function Song(id, singer, name, url){
 function initMunSongs() {
   var songList = new Array();
   
-  let song0 = new Song(0, '陈慧娴', '千千阕歌', 'http://fs.open.kugou.com/ef58542e3f62fe98ebf99df55cb0ccd3/5ca5e2b2/G126/M0A/12/11/vg0DAFpJ_WKAeoQNACcP2_ElSwo594.mp3');
+  let song0 = new Song(0, '陈慧娴', '千千阕歌', 'http://95.163.195.25/audio/if-you-were-a-boy.mp3');
   songList.push(song0);
   
-  let song1 = new Song(1, '黎瑞恩', '一人有一个梦想', 'http://fs.open.kugou.com/0d3144bbbed9d3196f04fdf4fa32a4ff/5ca5e2e6/G105/M02/09/05/CYcBAFvc3kOAPUUVADjDCMHBtaw694.mp3')
+  let song1 = new Song(1, '黎瑞恩', '一人有一个梦想', 'http://95.163.195.25/audio/if-you-were-a-boy.mp3')
   songList.push(song1);
   
-  let song3 = new Song(2, '谭咏麟', '讲不出再见', 'http://fs.open.kugou.com/dc7bc5a28be70384644157cd1d534dd8/5ca5e9f9/G142/M06/04/08/bpQEAFuKhOyAJS0sAEi4t--F9T0544.mp3')
+  let song3 = new Song(2, '谭咏麟', '讲不出再见', 'http://95.163.195.25/audio/if-you-were-a-boy.mp3')
   songList.push(song3);
   
-  let song4 = new Song(3, '关正杰', '俩忘烟水里', 'http://fs.open.kugou.com/ac8bf8890f1491594e7d92308a3da2bb/5ca5ea2e/G065/M01/17/19/gQ0DAFeaawSAKXWAAC-bCmXhS5Y344.mp3')
+  let song4 = new Song(3, '关正杰', '俩忘烟水里', 'http://95.163.195.25/audio/if-you-were-a-boy.mp3')
   songList.push(song4);
   
   return songList;
@@ -47,7 +47,7 @@ function initSonSongs() {
 //  let song0 = new Song(0, '儿童', '一闪一闪亮晶晶', 'http://fs.open.kugou.com/a9a121a28b149fd245065ac71b1a5c98/5ca5e242/G057/M0A/0E/00/eQ0DAFawAM-AeoQjABmRiFIBeq8524.mp3');
 //  songList.push(song0);
 
-  let song1 = new Song(1, '杨烁', '两只老虎', 'http://fs.open.kugou.com/60c8e9fc557a88166d44f686b945b011/5ca5e271/G124/M02/09/07/vA0DAFo7eXCAPSVPABGk6JgmntY967.mp3')
+  let song1 = new Song(1, '杨烁', '两只老虎', 'http://95.163.195.25/audio/if-you-were-a-boy.mp3')
   songList.push(song1);
 
   return songList;
